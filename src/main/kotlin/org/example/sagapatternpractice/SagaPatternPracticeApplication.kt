7ed41aa96b1c73dcba22ed39bdf6be266cde3233
seongkyu-lim <96x60812@gmail.com>
2025-09-30 20:33:27 +0900

    Chore: setting

---
+++ src/main/kotlin/org/example/sagapatternpractice/SagaPatternPracticeApplication.kt
@@ -0,0 +1,11 @@
+package org.example.sagapatternpractice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SagaPatternPracticeApplication
+
+fun main(args: Array<String>) {
+    runApplication<SagaPatternPracticeApplication>(*args)
+}
---
+++ src/test/kotlin/org/example/sagapatternpractice/SagaPatternPracticeApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.example.sagapatternpractice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SagaPatternPracticeApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
