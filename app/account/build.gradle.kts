0314ae96ebb51366e2a799dff14be34203213c85
seongkyu-lim <96x60812@gmail.com>
2025-10-02 16:37:11 +0900

    Chore: kafka

--- app/account/build.gradle.kts
+++ app/account/build.gradle.kts
@@ -11,8 +11,10 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.springframework.kafka:spring-kafka")
     runtimeOnly("com.h2database:h2")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+    testImplementation("org.springframework.kafka:spring-kafka-test")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
